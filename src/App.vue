285114ea8c67609c415b7d9741f08e18b357c822
paula970 <andrea.maldonado@udea.edu.co>
2020-09-19 10:00:16 -0500

    version1

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+  
+<template>
+   <v-app>
+    <Toolbar />
+    <v-main>
+       <Search v-on:searchResult="searchResult" />
+       <ImageList v-bind:items="items"/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Toolbar from "./components/Toolbar";
+import Search from "./components/Search";
+import ImageList from "./components/ImageList";
+export default {
+  name: 'App',
+  components: {
+    Toolbar,
+    Search,
+    ImageList
+  },
+  methods:{
+    searchResult(response){
+      this.items=response;
+    }
+  },
+  data: () => ({
+    items: [],
+  }),
+};
+</script>
---
+++ src/components/ImageList.vue
@@ -0,0 +1,12 @@
+  
+<template>
+    <v-card>
+        <h1>Aquí van las imagenes</h1>
+    </v-card>
+</template>
+
+<script>
+export default {
+    name : 'ImageList' 
+}
+</script>
